f3a1dd7e7c8d633727c7f7d9ae96fd3162f4e09d
Juan <juanmolmos@gmail.com>
2022-05-24 21:16:49 -0300

    primer commit proyecto recien descargado de initialzr solo se establecio la main app class en properties del proyecto

---
+++ src/main/java/com/yoprogramo/portfoliopersonal/PortfolioPersonalApplication.java
@@ -0,0 +1,13 @@
+package com.yoprogramo.portfoliopersonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioPersonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoprogramo/portfoliopersonal/PortfolioPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoprogramo.portfoliopersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
